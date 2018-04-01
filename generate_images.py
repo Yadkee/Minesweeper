c4753231f897fdb0231ba7f8c3b18b7d4e322b17
Yadkee <yadkee@gmail.com>
2018-04-02 01:54:39 +0200

    Changed the way I store the images in cache

--- generate_images.py
+++ generate_images.py
@@ -5,16 +5,19 @@ from PIL import Image
 SPRITES_PATH = join("media", "sprites.gif")
 SPR = Image.open(SPRITES_PATH)
 NAMES = ("cell.gif", "number.gif", "face.gif")
+NAMES = tuple("012345678bcrif_") + ("n0", "n1", "n2", "n3", "n4", "n5", "n6",
+                                    "n7", "n8", "n9", "clicked", "happy",
+                                    "undecise", "sad", "cool")
 
 
 def generate_images(factor=1):
     """Every image will be resized by a factor of 'factor'"""
     filePath = join("cache", str(factor), "")
-    groups = ([], [], [])
-    indexes = (0, 0, 1, 2)
     limits = ((0, 0, 144, 16, 16, 16), (0, 16, 96, 32, 16, 16),
               (0, 32, 128, 55, 13, 23), (0, 55, 128, 78, 26, 26))
-    for a, box in zip(indexes, limits):
+    images = []
+    a = 0
+    for box in limits:
         w, h = box[4], box[5]
         for y in range(*box[1::2]):
             for x in range(*box[::2]):
@@ -23,12 +26,11 @@ def generate_images(factor=1):
                     resized = cropped.resize((w * factor, h * factor))
                 else:
                     resized = cropped
-                groups[a].append(resized)
-    for name, group in zip(NAMES, groups):
-        n = name == "number"  # There is a weird transparency error in number
-        img = group[0 - 2 * n]
-        with open(filePath + name, "wb") as f:
-            img.save(f, save_all=True, append_images=group[1 - n:])
+                images.append(resized)
+                a += 1
+    for name, img in zip(NAMES, images):
+        with open(filePath + name + ".gif", "wb") as f:
+            img.save(f)
 
 if __name__ == "__main__":
     generate_images()
