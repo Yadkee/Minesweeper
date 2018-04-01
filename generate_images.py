5f234d1af41add5a9e55b15afec9c8aea5ad137e
Yadkee <yadkee@gmail.com>
2018-04-01 04:09:17 +0200

    Script to split sprites.gif into 3 different animated gifs

---
+++ generate_images.py
@@ -0,0 +1,32 @@
+#! python3
+from os.path import join
+from PIL import Image
+SPRITES_PATH = join("media", "sprites.gif")
+CACHE_PATH = join("cache", "")
+SPR = Image.open(SPRITES_PATH)
+
+
+def generate_images(factor=1):
+    groups = ([], [], [])
+    names = ("cell", "number", "face")
+    indexes = (0, 0, 1, 2)
+    limits = ((0, 0, 144, 16, 16, 16), (0, 16, 96, 32, 16, 16),
+              (0, 32, 128, 55, 13, 23), (0, 55, 128, 78, 26, 26))
+    for a, box in zip(indexes, limits):
+        w, h = box[4], box[5]
+        for y in range(*box[1::2]):
+            for x in range(*box[::2]):
+                cropped = SPR.crop((x, y, x + w, y + h))
+                if factor != 1:
+                    resized = cropped.resize((w * factor, h * factor))
+                else:
+                    resized = cropped
+                groups[a].append(resized)
+    for name, group in zip(names, groups):
+        n = name == "number"  # There is a weird transparency error in number
+        img = group[0 - 2 * n]
+        with open(CACHE_PATH + name + ".gif", "wb") as f:
+            img.save(f, save_all=True, append_images=group[1 - n:])
+
+if __name__ == "__main__":
+    generate_images()
