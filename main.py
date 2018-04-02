e587657e9d8a403cae3a90113ae7053d0c98bc21
Yadkee <yadkee@gmail.com>
2018-04-02 05:06:00 +0200

    Restructured and considered strange conditions that could sometimes happen

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from os.path import (
     exists)
 from json import load
 from re import match
+from os import mkdir
 
 basicConfig(format="%(asctime)s [%(levelname)s] %(name)s - %(message)s")
 logger = getLogger("main")
@@ -26,42 +27,45 @@ def prepare():
     # Load multiplicator factor from config.json
     with open("config.json") as f:
         loaded = load(f)
-        factor = int(loaded["resizeFactor"])  # I'm not acepting floats
+        factor = abs(loaded["resizeFactor"])
         difficultyIndex = str(loaded["difficulty"])
         difficulty = loaded[difficultyIndex]
     if not match(r"\d+x\d+ \d+ mines", difficulty):
         show_error("\nThe selected difficulty is not in the proper format:"
                    "\n\t[Width]x[Height] [number] mines")
-    folderPath = join("cache", str(factor))
+    if not exists("cache"):
+        mkdir("cache")
+        logger.info("Created cache folder")
     # Create cache for this factor
+    folderPath = join("cache", str(factor))
     if not exists(folderPath):
-        from os import mkdir
-        mkdir(folderPath)
-        hasPil = True
         if factor != 1:
             # Check if Pillow is installed (this may also recognise PIL but I
             #  trust users and think that they've correctly read dependencies)
             try:
                 from PIL import Image
             except ImportError:
-                hasPil = False
-                logger.warning("Pillow is not instaled")
+                if factor != 1:
+                    logger.warning("You chose a different size but Pillow is "
+                                   "not installed. Default size is going to "
+                                   "be used instead")
+                    factor = 1
+                    folderPath = join("cache", str(factor))
+            else:  # Generate images
+                mkdir(folderPath)
+                from generate_images import generate_images
+                generate_images(factor)
+                logger.info("Cached images for a factor of %d" % factor)
         # Copying from default
-        if factor == 1 or not hasPil:
+        if factor == 1 and not exists(folderPath):
+            mkdir(folderPath)
             from generate_images import NAMES as names
             from shutil import copyfile
-            if factor != 1:
-                logger.warning("You chose a different size but Pillow is not"
-                               "installed. Default size is going to be used")
             sourcePath = join("media", "default", "")
             destPath = join(folderPath, "")
             for name in names:
-                copyfile(sourcePath + name, destPath + name)
+                copyfile(sourcePath + name + ".gif", destPath + name + ".gif")
             logger.info("Cached images from default")
-        else:  # Generate images
-            from generate_images import generate_images
-            generate_images(factor)
-            logger.info("Cached images for a factor of %d" % factor)
     return factor, difficulty
 
 if __name__ == "__main__":
